f71c37989e48243ff5e84342f6e4d1e7d9ad689c
yasuki666 <1103831994@qq.com>
2019-10-07 12:50:47 +0800

    models update 6

--- small_CNN_practice/train1.py
+++ small_CNN_practice/train1.py
@@ -7,6 +7,10 @@ import torchvision.transforms as transforms
 
 import small_CNN_practice.net1 as net1
 net = net1.net
+checkpoint = torch.load(r'C:\Users\11038\PycharmProjects\small NN practice\models\Cifar10_model\6.ckpt')
+net.load_state_dict(checkpoint['net'])
+start_epoch = checkpoint['epoch']
+
 def main():
     batch_size = 4
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -15,14 +19,10 @@ def main():
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
 
-
-
-
-
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(),lr=0.001,momentum=0.9)
 
-    for epoch in range(5):
+    for epoch in range(7):
         train_loss = 0.0
         for batch_index, data in enumerate(train_loader,0):
 
@@ -46,7 +46,7 @@ def main():
                 'epoch':epoch+1
             }
 
-        torch.save(state,r'C:\Users\11038\PycharmProjects\small NN practice\models\Cifar10_model\%d.ckpt'%(epoch+1))
+        torch.save(state,r'C:\Users\11038\PycharmProjects\small NN practice\models\Cifar10_model\%d.ckpt'%(epoch+7))
         print('saved %d epoch'%(epoch+1))
     print('finish')
 
